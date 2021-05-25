ab7c26a5c75040a6a12eefeb1e39f1cb0bd8058c
Hannes <hannes@rang.de>
2021-05-25 18:44:32 +0200

    stripping down navbar

--- src/views/examples/Landing.js
+++ src/views/examples/Landing.js
@@ -38,7 +38,6 @@ import {
 
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
-import CardsFooter from "components/Footers/CardsFooter.js";
 
 class Landing extends React.Component {
   state = {};
