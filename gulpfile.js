327e3f7dc9f77edcac4e8b822dacfad864f65026
Hannes <hannes@rang.de>
2023-11-04 15:59:32 +0100

    change to trigger deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
-gulp.task("licenses", async function() {
-  // this is to add Creative Tim licenses in the production mode for the minified js
+gulp.task("licenses", async function () {
+  // this is to add Creative Tim licenses in the production mode for the minified js.
   await gulp
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(
